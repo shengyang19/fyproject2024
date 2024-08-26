5f48e3953f5ac67bee6872dd1a71ab5b5d054718
zuhdi04 <zuhdimuhd10@gmail.com>
2024-08-27 01:02:03 +0800

    cookie

---
+++ Palladium-Hotel/js/page-cookie.js
@@ -0,0 +1,27 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function deleteCookie(cname) {
+    const d = new Date();
+    d.setTime(d.getTime() - 1);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=;" + expires + ";path=/";
+}
+function checkCookie(cname) {
+    let user = getCookie("user");
+    if (user != "") {//if cookie exist
+        document.getElementById("user_title").innerHTML=user;
+    }
+    //else document.getElementById("user_title").innerHTML="NULL";   
+}
