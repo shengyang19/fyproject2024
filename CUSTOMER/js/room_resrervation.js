fea4bd23c277b6ea04aac145ad874e272a254517
zuhdi04 <zuhdimuhd10@gmail.com>
2024-11-03 03:23:47 +0800

    Update room_resrervation.js

--- CUSTOMER/js/room_resrervation.js
+++ CUSTOMER/js/room_resrervation.js
@@ -1,15 +1,58 @@
-let rooms = [{
-      "title": "Premier Park-View Room",
-      "image": "img/offers/2.png",
-      "description": [
-        "One king bed or two twin beds, One extra bed or one crib",
-        "King bed: 3 adults, or 2 adults and 1 child; Twin beds: 3 adults, or 2 adults and 2 children",
-        "KLCC Park and fountain"
-      ],
-      "booking_link": "#test-form"
-  }
+let rooms = [
+    {
+        "title": "Premier Park-View Room",
+        "price": ""
+    },
+    {
+        "title": "Pool Garden-View Room",
+        "price": ""
+    },
+    {
+        "title": "Park-View Room",
+        "price": ""
+    },
+    {
+        "title": "City-View Room",
+        "price": ""
+    },
+    {
+        "title": "Royal Suite",
+        "price": ""
+    },
+    {
+        "title": "Presidential Suite",
+        "price": ""
+    },
+    {
+        "title": "Ambassador Suite",
+        "price": ""
+    },
+    {
+        "title": "Palladium Park-View Suite",
+        "price": ""
+    },
+    {
+        "title": "Park-View Junior Suite",
+        "price": ""
+    },
+    {
+        "title": "Two-Bedroom Executive Park-View Apartment",
+        "price": ""
+    },
+    {
+        "title": "Two-Bedroom Premier Park-View Apartment",
+        "price": ""
+    },
+    {
+        "title": "Two-Bedroom Deluxe Apartment",
+        "price": ""
+    },
+    {
+        "title": "One-Bedroom Deluxe Apartment",
+        "price": ""
+    }
+];
 
-]
 
 function setupReservation(){
     let room = getCookie("booking");
