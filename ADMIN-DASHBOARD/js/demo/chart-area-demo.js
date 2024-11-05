d01f2a640037b30f3e1a0cc656cbf6d3c551b653
AzibFikri27 <muhdazibfikri@gmail.com>
2024-11-05 11:29:21 +0800

    update

--- ADMIN-DASHBOARD/js/demo/chart-area-demo.js
+++ ADMIN-DASHBOARD/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 10000, 25743, 11467, 7890, 10567, 15000, 25000, 36789, 34564, 23465, 23465],
     }],
   },
   options: {
